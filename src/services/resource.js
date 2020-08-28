f2ebb6661553d3da6def98175ecc8b2ecd48eaf5
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-08-28 14:32:22 +0300

    removing deleted files from tracking

--- src/services/resource.js
+++ src/services/resource.js
@@ -1,4 +1,4 @@
-class Resource {
+export default class Resource {
     // 'нижнее подчеркивание' показывает что это приватная переменная которая не должна использоваться вне класса'
     // ...=BIRD_NAME(по латинскому названию птицы)
 
@@ -6,7 +6,7 @@ class Resource {
     _urlImg = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=c48f2c557d4348c6afaa6bf1b364fb27&tag_mode=all&extras=url_m&format=json&nojsoncallback&tags=';
     _birdName = 'Troglodytes';
 
-    //Получение звуков
+    //асинхронная функция получения данных
     async getDataVoice(url) {
         //ответ от сервера(await - ждет результат промиса)
         const response = await fetch(url);
@@ -15,45 +15,51 @@ class Resource {
         if (!response.ok) {
             throw new Error(`Could not fetch Data Voice ${url}, received ${response.status}`)
         }
-
+        // debugger
         const result = await response.json();
 
         return result;
     }
 
-    //Получение картинок
     async getDataImg(url) {
         const ex = /[\^\{](.+[\^\}])/;
         const responce = await fetch(url);
 
         if (!responce.ok) {
-            throw new Error(`Could not fetch Data Img ${url}, received ${response.status}`)
+            throw new Error(`Could not fetch Data Img ${url}, received ${responce.status}`)
         }
 
-        //Ответ в виде текста
         const stringData = await responce.text();
 
-        //Преобразование ответа в JSON
         const result = JSON.parse(stringData.match(ex)[0]);
 
         return result
     }
 
-    getVoiceBird() {
-        return this.getDataVoice(`${this._urlVoice}${this._birdName}`);
+    async getVoiceBird(id, birdName = 'Troglodytes') {
+        // return this.getDataVoice(`${this._urlVoice}${this._birdName}`);
+        const result = await this.getDataVoice(`${this._urlVoice}${birdName}`);
+        console.log(result.recordings[id]);
+
+        return result.recordings[id]
     }
 
-    getImgBird() {
-        return this.getDataImg(`${this._urlImg}${this._birdName}`);
+    async getImgBird(id, birdName = 'Troglodytes') {
+        const result = await this.getDataImg(`${this._urlImg}${birdName}`);
+        return result.photos.photo[id]
     };
 }
 
-const res = new Resource();
+// const res = new Resource();
 
-res.getVoiceBird().then((body) => {
-    console.log(body);
-})
-
-res.getImgBird().then((body) => {
-    console.log(body);
-})
+// res.getVoiceBird(/*birdName*/).then((recordings) => {
+//     recordings.forEach(audio => {
+//         console.log(audio.file - name);
+//     })
+// })
+// debugger
+//В then приходит массив из функции getImgBird
+// res.getImgBird(id).then((photo) => {
+//     // console.log(photo['url_m']);
+//     return photo['url_m'];
+// })
