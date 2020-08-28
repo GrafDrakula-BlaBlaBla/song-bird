66a465475dcbad170df603360d8dda32787498c6
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-08-28 23:47:43 +0300

    feat: import component AnswerItems

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import AnswerItems from './AnswerItems/AnswerItems'
+
+
+import { randomArrBirdName } from '../../services/random';
 
 export default class Answer extends Component {
+
+
     render() {
         return (
             <div className='blockAnswer'>
                 <ul className='listAnswers'>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
+                    <AnswerItems />
                 </ul>
             </div>
         )
