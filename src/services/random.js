87eb043728b6bb93546f793b542156ecffdec841
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-08-28 23:44:01 +0300

    feat: create function randomArrBirdName()

--- src/services/random.js
+++ src/services/random.js
@@ -4,6 +4,11 @@ export const randomNumber = (max = 20) => {
     return Math.floor(Math.random() * (max - 1 + 1)) + 1;
 }
 
-export const randomName = (arr, lengthNewArr, fn) => {
+export const randomArrBirdName = (arrNameBirds, lengthNewArr = 5) => {
+    const length = arrNameBirds.length
+    const start = Math.floor(Math.random() * ((length - lengthNewArr) - 0 + 0)) + 0;
 
+    const newArray = arr.slice(start, lengthNewArr);
+
+    return newArray
 }
