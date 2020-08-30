a852b6b039b66a5b7488665c64ab3eb876209dd1
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-08-30 14:25:23 +0300

    add:  component Description

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Question from '../Question/Question'
+
+const Description = () => {
+    return (
+        <Question />
+    )
+}
+
+export default Description;
